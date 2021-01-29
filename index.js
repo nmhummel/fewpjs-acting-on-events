fe697f78148e2e4020126b3f1168201d66a58b0c
nmhummel <natalie.hummel@gmail.com>
2021-01-29 14:50:01 -0600

    commit tree 94360577e4131c1f3bb1db50d28ffe5822d8cc47

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+} 
+
+  
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+}); 
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0 && left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
